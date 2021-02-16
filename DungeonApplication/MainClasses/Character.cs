a9dc4056981de7969337c1f5085c27d37f09facb
Dillon Fisher <dillon.f12@hotmail.com>
2021-02-15 20:06:04 -0600

    Initial Commit - Added Base EMPTY Classes

---
+++ DungeonApplication/MainClasses/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClasses
+{
+    public abstract class Character
+    {
+        public string DefaultName { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public bool IsBoosted { get; set; }
+
+        public Character(string defaultName, string name, char gender, bool isBoosted)
+        {
+            DefaultName = defaultName;
+            Name = name;
+            Gender = gender;
+            IsBoosted = isBoosted;
+        }
+
+        public Character() { }
+    }
+}
---
+++ DungeonApplication/MainClasses/Monster_MovesEquipped.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClasses
+{
+    public class Monster_MovesEquipped
+    {
+        Monster_MovesEquipped() { }
+    }
+}
